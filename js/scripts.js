dead5be1ba06f7115955389e3b2bc0b4f1a3acb1
Claire Thorington <claire@Claires-iMac.local>
2022-06-01 09:18:33 -0700

    first JavaScript exercises

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,6 @@
-jQuery("h1").click(function() {
-  alert("This is a heading.");
-});
-
-jQuery("p").click(function() {
-  alert("This is a paragraph.");
-});
-
-jQuery("img").click(function() {
-  alert("This is an image.");
+$(document).ready(function() {
+  $(".clickable").click(function() {
+    $("#walrus-showing").toggle();
+    $("#walrus-hidden").toggle();
+  });
 });
